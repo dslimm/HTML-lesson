f3f05e3ba70fc6324bd4a8b96cd650e5547aa0a2
dslimm <dartanyan.fx@mail.ru>
2024-03-07 16:00:46 +0300

    новая ветка

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const color = ["зеленый", "синий", "красный", "серый"]
-const choice = ["нравится", "не нравится", "не очень нарвится"]
+const choice = ["нравится", "не нравится", "не очень нравится"]
 let randomColor = color[Math.floor(Math.random() * 4)]
 let randomChoice = choice[Math.floor(Math.random() * 3)]
 let result = "Мне " + randomChoice + " " + randomColor + " " + "цвет."
