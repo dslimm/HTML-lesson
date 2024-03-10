6185e2d5565c1692d8071e215d38055bbc475e17
dslimm <dartanyan.fx@mail.ru>
2024-03-10 13:56:55 +0300

    HTML-15: сверстал макет

--- main.js
+++
@@ -1,7 +0,0 @@
-const color = ["зеленый", "синий", "красный", "серый"]
-const choice = ["нравится", "не нравится", "не очень нравится"]
-let randomColor = color[Math.floor(Math.random() * color.length)]
-let randomChoice = choice[Math.floor(Math.random() * choice.length)]
-let result = ["Мне", randomChoice, randomColor, "цвет."]
-
-console.log(result.join(" "))
